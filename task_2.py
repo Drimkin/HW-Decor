1351adfd41b26f1b7b10cd5e2c80b3e2616417e3
Dmitry Zavialov <adminptk@mail.ru>
2024-01-06 17:54:29 +0300

    Initial commit

---
+++ task_2.py
@@ -0,0 +1,70 @@
+import os
+import time
+
+def logger(path):
+
+    def __logger(old_function):
+        def new_function(*args, **kwargs):
+            result = old_function(*args, **kwargs)
+
+            named_tuple = time.localtime()
+            time_string = time.strftime("%m/%d/%Y, %H:%M:%S", named_tuple)
+            function_name = old_function.__name__
+            args_str = ', '.join([repr(arg) for arg in args])
+            kwargs_str = ', '.join([f'{key}={repr(val)}' for key, val in kwargs.items()])
+
+            message = (f'Время вызова функции: {time_string}\n'
+                       f'Название функции: {function_name}\n'
+                       f'Аргументы функции args: {args_str} и kwargs: {kwargs_str}\n'
+                       f'Результат функции: {result}\n\n')
+
+            with open(path, 'a', encoding='UTF-8') as log_file:
+                log_file.write(message)
+            return result
+        return new_function
+
+    return __logger
+
+
+def test_2():
+    paths = ('log_1.log', 'log_2.log', 'log_3.log')
+
+    for path in paths:
+        if os.path.exists(path):
+            os.remove(path)
+
+        @logger(path)
+        def hello_world():
+            return 'Hello World'
+
+        @logger(path)
+        def summator(a, b=0):
+            return a + b
+
+        @logger(path)
+        def div(a, b):
+            return a / b
+
+        assert 'Hello World' == hello_world(), "Функция возвращает 'Hello World'"
+        result = summator(2, 2)
+        assert isinstance(result, int), 'Должно вернуться целое число'
+        assert result == 4, '2 + 2 = 4'
+        result = div(6, 2)
+        assert result == 3, '6 / 2 = 3'
+        summator(4.3, b=2.2)
+
+    for path in paths:
+
+        assert os.path.exists(path), f'файл {path} должен существовать'
+
+        with open(path) as log_file:
+            log_file_content = log_file.read()
+
+        assert 'summator' in log_file_content, 'должно записаться имя функции'
+
+        for item in (4.3, 2.2, 6.5):
+            assert str(item) in log_file_content, f'{item} должен быть записан в файл'
+
+
+if __name__ == '__main__':
+    test_2()
